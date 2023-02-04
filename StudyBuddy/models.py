20e0302f7b139bebd7464e07b84c42c0340413d6
Paxton Schweigert <paxmanrocks@gmail.com>
2023-02-04 08:36:34 -0800

    Add some serializers for our models so far and fix some errors in models.py

--- StudyBuddy/models.py
+++ StudyBuddy/models.py
@@ -2,32 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Amentity():
+class Amenity:
     description = models.CharField()
 
 
 class Comment(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
 
     class Meta:
         ordering = ['created_on']
 
 
-class Locations(models.Model):
+class Location(models.Model):
     longitude = models.CharField(max_length=50)
     latitude = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
 
 
-class Events(models.Model):
+class Event(models.Model):
     creator = models.CharField(max_length=50)  # this should be something else
-    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
-    creationTime = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    creation_time = models.DateTimeField(auto_now_add=True)
     meetup_time = models.DateTimeField()
     comments = models.ManyToManyField(Comment)
 
 
-class StudyGroup(Events):
-    amentities = models.ManyToManyField(Amentity)
+class StudyGroup(Event):
+    amentities = models.ManyToManyField(Amenity)
 
 
